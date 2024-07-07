324e65db3ddf18e0e0735ddca9c4c407118234bd
faizan <faizansohail077@gmail.com>
2024-07-07 17:17:06 +0500

    added clerk user auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { useAuth, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
 import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>LandingPage
+
+      <div className="flex items-center gap-5">
+        <Link href="/sign-in">
+          <Button>Sign In</Button>
+        </Link>
+      </div>
+
+
+    </div>
   )
 }
 
